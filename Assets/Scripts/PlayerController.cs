f062904d39ea3e58e946f9961658c9fa7614593c
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-09-24 19:48:35 -0500

    Counting Collectibles and Displaying Score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb2d;
     public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     private void Start()
     {
+
+        SetCountText();
+        count = 0;
+        winText.text = "";
         rb2d = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
@@ -25,7 +33,18 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+           
+        }
+    }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count>=9)
+        {
+            winText.text = "You win!";
         }
     }
 }
